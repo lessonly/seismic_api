be07cb2beb550e704ea9338d940591c6e50ea5f2
Ross Reinhardt <rreinhardt9@gmail.com>
2020-03-23 17:04:24 -0400

    Add initial endpoints for teamsites

--- lib/seismic_api.rb
+++ lib/seismic_api.rb
@@ -4,6 +4,27 @@ require "forwardable"
 
 module SeismicAPI
   class Error < StandardError; end
+
+  module_function def configuration
+    @configuration ||= Configuration.new
+  end
+
+  module_function def config
+    configuration
+  end
+
+  module_function def configure
+    yield configuration
+  end
+
+  class Configuration
+    attr_accessor :base_url
+
+    def initialize(args = {})
+      @base_url = args.fetch(:base_url) { "https://api.seismic.com/integration/v2" }
+    end
+  end
+
   class Response
     attr_reader :raw_response
     extend Forwardable
@@ -14,11 +35,45 @@ module SeismicAPI
     end
 
     def success?
-      @raw_response.is_a?(Net::HTTPOK)
+      @raw_response.is_a?(Net::HTTPSuccess)
     end
 
     def body
       JSON.parse(@raw_response.body)
+    end
+  end
+
+  class Request
+    def initialize(oauth_token: nil)
+      @oauth_token = oauth_token
+    end
+
+    def get(uri)
+      uri = URI.parse(uri)
+      req = Net::HTTP::Get.new(uri)
+      if @oauth_token
+        req['Authorization'] = "Bearer #{@oauth_token}"
+      end
+
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+
+      Response.new(res)
+    end
+
+    def post(uri, body: nil)
+      uri = URI.parse(uri)
+      req = Net::HTTP::Post.new(uri)
+      req['Authorization'] = "Bearer #{@oauth_token}"
+      req.body = body.to_json if body
+      req['Content-Type'] = 'application/json'
+
+      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(req)
+      end
+
+      Response.new(res)
     end
   end
 
@@ -27,53 +82,92 @@ module SeismicAPI
       @oauth_token = oauth_token
     end
 
-    def teamsites(id: nil)
-      Teamsites.new(oauth_token: @oauth_token, id: id)
+    def teamsites
+      Teamsites.new(oauth_token: @oauth_token)
     end
   end
 
   class Teamsites
-    def initialize(oauth_token:,id:nil)
+
+    # Initialize teamsites client
+    #
+    # @param oauth_token [String] valid oauth token
+    #
+    # @return [self]
+    def initialize(oauth_token:)
       @oauth_token = oauth_token
-      @id = id
+      @base_url = SeismicAPI.config.base_url + "/teamsites"
     end
 
+    # Return all teamsites
+    #
+    # @return [SeismicAPI::Response]
     def all
-      uri = URI.parse("https://api.seismic.com/integration/v2/teamsites")
-      req = Net::HTTP::Get.new(uri)
-      req['Authorization'] = "Bearer #{@oauth_token}"
-
-      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-        http.request(req)
-      end
-
-      Response.new(res)
+      request.get(teamsites_url)
     end
 
-    def find(id: @id)
-      uri = URI.parse("https://api.seismic.com/integration/v2/teamsites/#{id}")
-      req = Net::HTTP::Get.new(uri)
-      req['Authorization'] = "Bearer #{@oauth_token}"
-
-      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-        http.request(req)
-      end
-
-      Response.new(res)
+    # Find a specific teamsite
+    #
+    # @param id [String] id of teamsite
+    #
+    # @return [SeismicAPI::Response]
+    def find(id:)
+      request.get("#{teamsites_url}/#{id}")
     end
 
-    def add_folder(teamsite_id: @id, name:, parent_folder_id: "root")
-      uri = URI.parse("https://api.seismic.com/integration/v2/teamsites/#{teamsite_id}/folders")
-      req = Net::HTTP::Post.new(uri)
-      req['Authorization'] = "Bearer #{@oauth_token}"
-      req.body = { name: name, parentFolderId: parent_folder_id }.to_json
-      req['Content-Type'] = 'application/json'
+    # Create a folder in the teamsite
+    #
+    # @param teamsiteId [String] ID of teamsite
+    # @param name [String] name of folder
+    # @param parentFolderId [String] ID of parent folder (it not supplied,
+    #   it goes into the root directory for that teamsite)
+    #
+    # @return [SeismicAPI::Response]
+    def add_folder(teamsiteId:, name:, parentFolderId: "root")
+      request.post(
+        "#{teamsites_url}/#{teamsiteId}/folders",
+        body: { name: name, parentFolderId: parentFolderId }
+      )
+    end
 
-      res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-        http.request(req)
-      end
+    # Add a url as content
+    #
+    # @param teamsiteId [String] ID of teamsite
+    # @param name [String] name to be displayed on the URL content card
+    # @param parentFolderId [String] ID of folder to place content in
+    # @param url [Hash] Object representing URL to be added
+    #   * :url [String] (required) the actual URL the content should link to
+    #   * :openInNewWindow [Boolean] (false) Should the content open in a new window
+    #
+    # @return [SeismicAPI::Response]
+    def add_url(teamsiteId:, name:, url:, **args)
+      request.post(
+        "#{teamsites_url}/#{teamsiteId}/urls",
+        body: { url: url, name: name, **args }
+      )
+    end
 
-      Response.new(res)
+    # Publish given library content
+    #
+    # @param teamsiteId [String] ID of teamsite
+    # @param content [Array<{id=>String}>] ids for content to be published
+    #
+    # @return [SeismicAPI::Response]
+    def publish(teamsiteId:, content:, **args)
+      request.post(
+        "#{teamsites_url}/#{teamsiteId}/publish",
+        body: { content: content, **args }
+      )
+    end
+
+    private
+
+    def request
+      Request.new(oauth_token: @oauth_token)
+    end
+
+    def teamsites_url
+      SeismicAPI.config.base_url + "/teamsites"
     end
   end
 end
--- test/seismic_api_test.rb
+++ test/seismic_api_test.rb
@@ -5,10 +5,6 @@ class SeismicApiTest < Minitest::Test
     refute_nil ::SeismicApi::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
-  end
-
   def test_getting_teamsites
     stub = stub_request(:get, "https://api.seismic.com/integration/v2/teamsites")
       .with(headers: { 'Authorization' => 'Bearer someoauthtoken' })
@@ -38,9 +34,104 @@ class SeismicApiTest < Minitest::Test
         body: { name: "Lessonly", parentFolderId: "root" }
       ).to_return(body: { "id" => 32, "name" => "Lessonly" }.to_json)
 
-    response = SeismicAPI::Client.new(oauth_token: "someoauthtoken").teamsites(id: 32).add_folder(name: "Lessonly")
+    response = SeismicAPI::Client.new(oauth_token: "someoauthtoken").teamsites.add_folder(
+      teamsiteId: "32",
+      name: "Lessonly"
+    )
 
     assert_requested(stub)
     assert_equal({ "id" => 32, "name" => "Lessonly" }, response.body)
+  end
+
+  def test_add_url_content_to_teamsite
+    stub = stub_request(:post, "#{teamsites_url}/32/urls")
+      .with(
+        headers: common_post_headers,
+        body: {
+          url: { url: "www.google.com", openInNewWindow: false },
+          name: "Imma Test",
+          description: "Itsa Test",
+          parentFolderId: "1234",
+          externalId: "Lesson/42"
+        }
+      ).to_return(body: {
+        "id" => 32,
+        "url" => { "url" => "www.google.com", "openInNewWindow" => false },
+        "name" => "Imma Test",
+        "description" => "Itsa Test",
+        "externalId" => "Lesson/42"
+      }.to_json)
+
+    response = SeismicAPI::Teamsites.new(oauth_token: "someoauthtoken")
+      .add_url(
+        teamsiteId: 32,
+        url: { url: "www.google.com", openInNewWindow: false },
+        name: "Imma Test",
+        description: "Itsa Test",
+        parentFolderId: "1234",
+        externalId: "Lesson/42"
+      )
+
+    assert_requested(stub)
+    assert_equal({
+      "id" => 32,
+      "url" => { "url" => "www.google.com", "openInNewWindow" => false },
+      "name" => "Imma Test",
+      "description" => "Itsa Test",
+      "externalId" => "Lesson/42"
+    }, response.body)
+  end
+
+  def test_publish
+    stub = stub_request(:post, "#{teamsites_url}/32/publish")
+      .with(
+        headers: common_post_headers,
+        body: {
+          content: [
+            { id: "831" }
+          ]
+        }
+      ).to_return(body: {
+        "totalRequests" => "1",
+        "totalErrors" => "0",
+        "totalSucceeded" => "1",
+        "totalWarnings" => "0",
+        "errors" => [],
+        "warnings" => []
+      }.to_json)
+
+    response = SeismicAPI::Teamsites.new(oauth_token: "someoauthtoken")
+      .publish(
+        teamsiteId: 32,
+        content: [
+          { id: "831" }
+        ]
+      )
+
+    assert_requested(stub)
+    assert_equal({
+      "totalRequests" => "1",
+      "totalErrors" => "0",
+      "totalSucceeded" => "1",
+      "totalWarnings" => "0",
+      "errors" => [],
+      "warnings" => []
+    }, response.body)
+  end
+
+  def base_url
+    "https://api.seismic.com/integration/v2"
+  end
+
+  def teamsites_url
+    base_url + "/teamsites"
+  end
+
+  def common_headers
+    { 'Authorization' => 'Bearer someoauthtoken' }
+  end
+
+  def common_post_headers
+    common_headers.merge({ 'content-type' => 'application/json' })
   end
 end
